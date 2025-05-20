b0ec9bc0b8ed883f59ed23a80b509f3579419d42
LetusCasa13 <lgonzalez@elpuig.xeill.net>
2025-05-20 19:28:03 +0200

    Conexión Higernate.

--- src/test/java/Backend/MemoryControllerTest.java
+++ src/test/java/Backend/MemoryControllerTest.java
@@ -26,11 +26,37 @@ class MemoryControllerTest {
 
     @Test
     void nouContacte() {
+        MemoryController controlador= new MemoryController();
+        Contacte c1 = controlador.nouContacte("Miguel", "Rodriguez", "239765490", "miguel@example.com");
 
+        //verifico si se creó
+
+        assertNotNull(c1, "El contacte no hauria de ser null");
+        assertEquals("Miguel", c1.getNom(), "Nom incorrecte");
+        assertEquals("Rodriguez", c1.getCognoms(), "Cognoms incorrectes");
+        assertEquals("239765490", c1.getTelefon(), "Telefon incorrecte");
+        assertEquals("miguel@example.com", c1.getEmail(), "Email incorrecte");
     }
+
+
+
 
     @Test
     void actualitzarContacte() {
+        MemoryController controlador = new MemoryController();
+        Contacte c = controlador.nouContacte("Elena", "Suarez", "123456789", "elena@example.como");
+        int id = c.getId();
+
+        Contacte actualitzat = controlador.actualitzarContacte(id, "Elena", "López", "987654321", "elena.lopez@example.com");
+
+       //verificamos q el contactos se ha actualizado
+
+        assertNotNull(actualitzat, "El contacte hauria d'existir");
+        assertEquals("Elena", actualitzat.getNom(), "Nom no actualitzat correctament");
+        assertEquals("López", actualitzat.getCognoms(), "Cognoms no actualitzat correctament");
+        assertEquals("987654321", actualitzat.getTelefon(), "Telèfon no actualitzat correctament");
+        assertEquals("elena.lopez@example.com", actualitzat.getEmail(), "Email no actualitzat correctament");
+
 
     }
 
